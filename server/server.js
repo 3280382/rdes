b0a0795940fcbf31a36e64301c0a96f33b48f547
3280382@qq.com <3280382@qq.com>
2016-09-29 17:44:02 +0800

    ws es

--- server/server.js
+++ server/server.js
@@ -1,9 +1,40 @@
+var fs = require('fs');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.send('<h1>Welcome Realtime Server</h1>');
+	res.send('<h1>Welcome Realtime Server：</h1>');
+});
+
+app.get('/es/:fileName', function(req, res){
+	fs.readFile('./es/'+fileName, function (err, content) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(content, 'utf-8');
+        });
+}); 
+
+app.get('/ws/index.html', function(req, res){
+	fs.readFile('./ws/index.html', function (err, content) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(content, 'utf-8');
+        });
+});        
+
+app.get('/msg', function(req, res){
+	    		console.log(req.headers['user-agent']);
+        // 1. 设定头信息
+        res.writeHead(200, {
+            "Content-Type": "text/event-stream",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive"
+        });
+
+        // 2. 输出内容，必须 "data:" 开头 "\n\n" 结尾（代表结束）
+        setInterval(function () {
+            res.write("data: " + Date.now() + "\n\n");
+            console.log(req.headers['user-agent']);
+        }, 1000);
 });
 
 //在线用户
--- server/ws/client.js
+++ server/ws/client.js
@@ -97,7 +97,7 @@
 			this.scrollToBottom();
 			
 			//连接websocket后端服务器
-			this.socket = io.connect('ws://192.168.31.145:3001');
+			this.socket = io.connect('ws://127.0.0.1:3001');
 			
 			//告诉服务器端有用户登录
 			this.socket.emit('login', {userid:this.userid, username:this.username});
